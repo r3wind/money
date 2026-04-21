8fd2efe27f9379df78ddf8b19e74f0f44597c0bc
Richard Stewart <Richard.Stewart@newcastle.co.uk>
2026-04-21 07:37:32 +0100

    small tweaks

--- FinanceProject/Models/SavingsPot.cs
+++ FinanceProject/Models/SavingsPot.cs
@@ -6,7 +6,11 @@ namespace FinanceProject.Models
     {
         [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; } = string.Empty;
+
         [Range(0.01, double.MaxValue, ErrorMessage = "Amount must be greater than zero")]
         public decimal Amount { get; set; }
+
+        [Range(0, 100, ErrorMessage = "AER must be between 0 and 100")]
+        public decimal Aer { get; set; }
     }
 }
